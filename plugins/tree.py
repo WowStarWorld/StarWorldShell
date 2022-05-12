624ea302af64bc351d65be7341aad18c536e3e96
WowStarWorld <starworldstudio1@gmail.com>
2022-05-12 16:24:59 +0800

    Create tree.py

---
+++ plugins/tree.py
@@ -0,0 +1,42 @@
+import os
+import rich.console
+console = rich.console.Console()
+
+def print_dir(path, level=0):
+    try:
+        for file in os.listdir(path):
+            if os.path.isdir(os.path.join(path, file)):
+                console.log("│  " * level + "├─" + file)
+                print_dir(os.path.join(path, file), level + 1)
+            else:
+                console.log("│  " * level + "├─" + file)
+    except:pass
+
+def print_dir_command(*args) -> None:
+    if args == ():
+        print_dir(os.getcwd())
+    else:
+        print_dir(args[0])
+
+info = {
+    "name": "SWTree",
+    "author": "WoWStarWorld",
+    "description": "Display directory tree",
+    "version": "1.0",
+    "website": "https://github.com/WowStarWorld",
+    "license": "MIT License",
+    "commands": [
+        {
+            "name": "tree",
+            "function": print_dir_command,
+            "description": "Print dir tree",
+            "usage": "tree | tree <path>"
+        }
+    ]
+}
+
+def onLoad(*args) -> None:
+    console.log("[green]\[SWTree] Enabled !")
+
+def onDisable(*args) -> None:
+    console.log("[red]\[SWTree] Disabled !")
